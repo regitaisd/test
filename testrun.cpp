7b818e9035c5420c73684689a4b71c7e0436bac6
Regita I <regitaisd@gmail.com>
2016-10-05 08:59:13 +0700

    Add files via upload

---
+++ testrun.cpp
@@ -0,0 +1,15 @@
+//programming challenge no.6: annual pay
+
+#include <iostream>
+using namespace std;
+
+int main (){
+	int payAmount = 2200;
+	int payPeriod = 26;
+	int annualPay = payAmount * payPeriod;
+	
+	cout << "The amount the employee gets paid is: $ " << payAmount << "\n";
+	cout << "The number of times the employee gets paid is: " << payPeriod << " times\n";
+	cout << "The annual pay the employee gets is: $ " << annualPay;
+	return 0;
+}
